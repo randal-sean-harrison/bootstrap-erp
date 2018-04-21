4e9589c6e2e46a1fa71663585f9486e0c3c8c76a
Randal Sean Harrison <rharriso@nd.edu>
2018-04-21 13:48:00 -0400

    updates to the main pages

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Add the copyright date into the footer with jquery
+var thisYear = (new Date).getFullYear();
+$("#this-year").text(thisYear);
+
+
+// Prepares print stylesheet by hiding and unhiding elements
+$("#print-this-page").on("click", function(){
+
+   // open all panels for printing
+   $(".panel-collapse").addClass("in");
+
+   // print the guide header and page header
+   $("#print-headings").removeClass("hidden");
+
+   // hide the breadcrumbs
+   $("#breadcrumb-nav").addClass("hidden");
+
+   // style the panels
+
+});
